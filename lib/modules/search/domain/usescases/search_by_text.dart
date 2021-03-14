df7f1bacb6e68ccfcfa9550906bd8a5bb2a06a2c
WSixx <lucas.2007s@gmail.com>
2021-03-14 15:08:51 -0300

    Domain pronto

--- lib/modules/search/domain/usescases/search_by_text.dart
+++ lib/modules/search/domain/usescases/search_by_text.dart
@@ -1,7 +1,23 @@
 import 'package:flutter_clean_arch_flutterando/modules/search/domain/entities/result_search.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_clean_arch_flutterando/modules/search/domain/errors/errors.dart';
+import 'package:flutter_clean_arch_flutterando/modules/search/domain/repositories/search_repository.dart';
 
 abstract class SearchByText {
-  Either<FailureSearch, Future<List<ResultSearch>>> call(String searchText);
+  Future<Either<FailureSearch, List<ResultSearch>>> call(String searchText);
+}
+
+class SearchByTextImpl implements SearchByText {
+  final SearchRepository repository;
+
+  SearchByTextImpl(this.repository);
+
+  @override
+  Future<Either<FailureSearch, List<ResultSearch>>> call(
+      String searchText) async {
+    if (searchText == null || searchText.isEmpty) {
+      return Left(InvalidTextError());
+    }
+    return repository.search(searchText);
+  }
 }
